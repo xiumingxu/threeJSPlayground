e311f21f6c9ce970177552d6cc907234a0c4c48a
XIX <xiuming.x@outlook.com>
2020-08-23 11:55:29 -0700

    clean up

---
+++ msJump/game.js
@@ -0,0 +1,16 @@
+// import './js/libs/weapp-adapter'
+// import './js/libs/symbol'
+
+import Main from './src/main.js'
+
+class Game{
+    constructor(){
+        this.Main = Main;
+        console.log("this is a game")
+
+    }
+}
+
+
+export default new Game()
+
---
+++ msJump/src/main.js
@@ -0,0 +1,7 @@
+import * as THREE from '../libs/three.module.js' 
+class Main{
+    constructor(){
+        console.log("main");
+    }
+}
+export default new Main();
